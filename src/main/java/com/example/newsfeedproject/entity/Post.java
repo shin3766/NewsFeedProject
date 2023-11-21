34b4f5f0f345626f2e984f522e2dba682c211ed1
yudonggeun <67232422+yudonggeun@users.noreply.github.com>
2023-11-21 19:34:19 +0900

    feat : 게시글 클래스 골격 구현
    게시글 controller, service, repository, entity의 골격을 구현했습니다.

---
+++ src/main/java/com/example/newsfeedproject/entity/Post.java
@@ -0,0 +1,36 @@
+package com.example.newsfeedproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime activatedAt;
+
+    @Builder
+    private Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
