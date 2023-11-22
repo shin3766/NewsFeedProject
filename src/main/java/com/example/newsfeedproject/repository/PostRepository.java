f8cf11b57f7ef9b2fc580eb9e4724076fc7171e5
Yoosup Shin <brian96shin@gmail.com>
2023-11-22 11:02:35 +0900

    Service에서 findPost, updatePost, getPosts 구현

--- src/main/java/com/example/newsfeedproject/repository/PostRepository.java
+++ src/main/java/com/example/newsfeedproject/repository/PostRepository.java
@@ -3,5 +3,8 @@ package com.example.newsfeedproject.repository;
 import com.example.newsfeedproject.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByOrderByCreatedAtDesc();
 }
--- src/main/java/com/example/newsfeedproject/service/PostService.java
+++ src/main/java/com/example/newsfeedproject/service/PostService.java
@@ -6,6 +6,9 @@ import com.example.newsfeedproject.entity.Post;
 import com.example.newsfeedproject.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -13,6 +16,7 @@ public class PostService {
 
     private final PostRepository postRepository;
 
+    // post 등록하기
     public PostResponseDto createPost(PostRequestDto requestDto){
         // 새로운 post객체에 requestDto 넣기
         Post post = new Post(requestDto);
@@ -24,5 +28,28 @@ public class PostService {
         PostResponseDto postResponseDto = new PostResponseDto(savePost);
 
         return postResponseDto;
+    }
+
+    // post 전체 조회
+    public List<PostResponseDto> getPosts() {
+        return postRepository.findAllByOrderByCreatedAtDesc().stream().map(PostResponseDto::new).toList();
+    }
+
+    @Transactional
+    public Long updatePost(Long id, PostRequestDto requestDto) {
+        // DB에 해당 post 있는지 확인
+        Post post = findPost(id);
+
+        // requestDto로 post객체 업데이트
+        post.update(requestDto);
+
+        //id 반환
+        return id;
+    }
+
+    private Post findPost(Long id) {
+        return postRepository.findById(id).orElseThrow(() ->
+            new IllegalArgumentException("선택한 게시글은 존재하지 않습니다.")
+        );
     }
 }
