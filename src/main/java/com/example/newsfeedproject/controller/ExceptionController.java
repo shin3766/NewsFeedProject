46beb551c295301f97169651309a67e524af1387
yudonggeun <ydong98@gmail.com>
2023-11-24 12:38:44 +0900

    fix : 이메일 전송 실패 예외 상태 코드 수정
    이메일 전송 실패 오류 발생시 INTERNAL SERVER ERROR를 반환하도록 수정했습니다.

--- src/main/java/com/example/newsfeedproject/controller/ExceptionController.java
+++ src/main/java/com/example/newsfeedproject/controller/ExceptionController.java
@@ -2,13 +2,16 @@ package com.example.newsfeedproject.controller;
 
 import com.example.newsfeedproject.dto.MessageDto;
 import jakarta.mail.SendFailedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler({SendFailedException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public MessageDto sendFail() {
         return new MessageDto("이메일 전송에 실패했습니다.");
     }
