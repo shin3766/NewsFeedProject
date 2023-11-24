517d38f6d98629fe9afb89fccd4c96348acc5b8e
yudonggeun <ydong98@gmail.com>
2023-11-24 20:54:13 +0900

    test : 댓글 수정 테스트 작성
    1. 성공 테스트 작성
    2. 로그인 하지 않은 경우 테스트 작성
    3. 자신이 작성하지 않은 댓글 수정 실패 테스트 작성

--- src/test/java/com/example/newsfeedproject/controller/CommentControllerTest.java
+++ src/test/java/com/example/newsfeedproject/controller/CommentControllerTest.java
@@ -2,8 +2,11 @@ package com.example.newsfeedproject.controller;
 
 import com.example.newsfeedproject.IntegrationTest;
 import com.example.newsfeedproject.dto.CreateCommentRequest;
+import com.example.newsfeedproject.dto.UpdateCommentRequest;
+import com.example.newsfeedproject.entity.Comment;
 import com.example.newsfeedproject.entity.Post;
 import com.example.newsfeedproject.entity.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static com.example.newsfeedproject.entity.UserRole.USER;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.securityContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -45,6 +49,7 @@ class CommentControllerTest extends IntegrationTest {
                 )
                 .andDo(print())
                 .andExpectAll(
+                        status().isOk(),
                         jsonPath("$.id").exists(),
                         jsonPath("$.content").value(request.content()),
                         jsonPath("$.author").value(user.getUsername()),
@@ -69,4 +74,65 @@ class CommentControllerTest extends IntegrationTest {
                 );
     }
 
+    @DisplayName("댓글 수정 성공")
+    @Test
+    void updateComment() throws Exception {
+        // given
+        Comment comment = saveComment("test comment", user, post);
+        var request = new UpdateCommentRequest(comment.getId(), "after");
+        SecurityContext context = contextJwtUser(user.getId(), user.getUsername(), user.getRole());
+        // when // then
+        mockMvc.perform(patch("/api/v1/comment")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request))
+                        .with(securityContext(context))
+                )
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.id").value(comment.getId()),
+                        jsonPath("$.content").value(request.content()),
+                        jsonPath("$.author").value(user.getUsername()),
+                        jsonPath("$.createdAt").exists()
+                );
+    }
+
+    @DisplayName("로그인 하지 않은 경우 댓글 수정 실패")
+    @Test
+    void updateCommentFailWhenNotLogin() throws Exception {
+        // given
+        Comment comment = saveComment("test comment", user, post);
+        var request = new UpdateCommentRequest(comment.getId(), "after");
+        // when // then
+        mockMvc.perform(patch("/api/v1/comment")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpectAll(
+                        status().isForbidden(),
+                        jsonPath("$.message").value("권한이 없습니다.")
+                );
+    }
+
+    @DisplayName("자신이 작성하지 않은 댓글 수정 실패")
+    @Test
+    void updateCommentWhenLoginUserIsNotAuthor() throws Exception {
+        // given
+        User author = saveUser("정석", "1234", "tes@fd.com", USER);
+        Comment comment = saveComment("test comment", author, post);
+        var request = new UpdateCommentRequest(comment.getId(), "after");
+        SecurityContext context = contextJwtUser(user.getId(), user.getUsername(), user.getRole());
+        // when // then
+        mockMvc.perform(patch("/api/v1/comment")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request))
+                        .with(securityContext(context))
+                )
+                .andDo(print())
+                .andExpectAll(
+                        status().isForbidden(),
+                        jsonPath("$.message").value("댓글 수정 권한이 없습니다.")
+                );
+    }
 }
